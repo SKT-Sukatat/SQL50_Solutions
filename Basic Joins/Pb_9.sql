6356de4896dc21313d34c1426eac58a8fd31c59e
SKT-Sukatat <82657530+SKT-Sukatat@users.noreply.github.com>
2024-08-02 15:19:05 +0700

    Update Solution2 to Pb_9

--- Basic Joins/Pb_9.sql
+++ Basic Joins/Pb_9.sql
@@ -1,5 +1,17 @@
-# Write your MySQL query statement below
+-- Solution 1 By _Tanmay
+-- Write your MySQL query statement below
 
 SELECT w1.id
 FROM Weather w1, Weather w2
 WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
+
+
+
+-- Soluntion 2 By himanshu__mehra__
+-- Write your MySQL query statement below
+
+SELECT w2.id
+FROM weather w1
+JOIN weather w2
+ON DATEDIFF (w1.recordDate ,w2.recordDate ) = -1 -- YESTERDAY - TODAY = -1
+AND w2.temperature > w1.temperature; -- TODAY.Tempearture > YESTERDAY.Temperature
